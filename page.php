90470738180001231b7ffe4ac74c4f2ccb5b1fbf
sarah semark <sarah@triggersandsparks.com>
2015-09-21 18:19:54 +0000

    Remove book limit from bibliography page and tweak padding.

--- page.php
+++ page.php
@@ -14,25 +14,25 @@
 
 get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				
+
 				<article id="post-<?php the_ID(); ?>" <?php post_class('grid_6 push_1 alpha'); ?>>
-				
-				
-				<?php if ( is_front_page() ) { 
-				  remove_filter ('the_content', 'wpautop'); 
+
+
+				<?php if ( is_front_page() ) {
+				  remove_filter ('the_content', 'wpautop');
 				?>
-					
-				<?php } else { ?>	
-					<h1 class="entry-title"><?php the_title(); ?></h1>	
+
+				<?php } else { ?>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
 				<?php } ?>
-				
-				
-				  
-   
+
+
+
+
    <?php if (is_page('bibliography')) { ?>
-   
+
   <?php // let's list all the series
-  
+
   $args = array( 'taxonomy' => 'series', 'exclude' => '34' );
   //$terms = get_terms('series', $args);
   $terms = apply_filters( 'taxonomy-images-get-terms', null, $args);
@@ -46,10 +46,10 @@ get_header(); ?>
     	               <div class="preview'. $author.'">
     	               <h2><a href="/series/'.$term->slug.'" title="' . sprintf(__('Books in the %s series', 'my_localization_domain'), $term->name) . '">'.$term->name.'</a></h2>
                     ';
-                    
-    // and books in the series, too!           
+
+    // and books in the series, too!
       $args = array(
-      'numberposts' => 8,
+      'numberposts' => -1,
       'order' => 'ASC',
       'post_type' => 'books',
       'series' => $term->slug,
@@ -59,33 +59,33 @@ get_header(); ?>
       // print_r($books);
       foreach ($books as $book) { ?>
         	<li><a href="/books/<?php echo $book->post_name; ?>"><?php echo $book->post_title; ?></a></li>
-        	
+
       <?php } ?>
-      
+
       </div></article>
-      
+
       <?php } ?>
 
-   
+
    <?php } ?>
-				
 
-				
 
-				
-				
+
+
+
+
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
 						<?php //edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
-				
-				
-				
+
+
+
 				<?php if ( is_front_page() ) { ?>
-				
-				<?php // get featured book					
+
+				<?php // get featured book
       		$args = array(
          'numberposts' => 1,
          'post_type' => 'books',
@@ -93,7 +93,7 @@ get_header(); ?>
          'post_status' => 'publish'
         );
           $show_book = get_posts ($args);
-            foreach ($show_book as $post) :  setup_postdata($post);  
+            foreach ($show_book as $post) :  setup_postdata($post);
   	          // determine which author is being used!
               $author = get_post_meta($post->ID, 'wpcf-author', true);
               if ($author == "1") {
@@ -105,26 +105,26 @@ get_header(); ?>
               } elseif ($author == "3") {
                 $author = "sagarawest";
                 $authorname = "Michelle Sagara West";
-              } 
+              }
             ?>
-            			
-			<div class="grid_6 push_1 alpha">		
+
+			<div class="grid_6 push_1 alpha">
 		  <?php the_post_thumbnail('book-preview', array('class' => 'feature'));?>
         <div class="preview <?php echo $author; ?>">
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>    
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <h4><?php echo get_post_meta($post->ID, 'wpcf-subtitle', true) ; ?><br />
-          by <?php echo $authorname; ?></h4>      
+          by <?php echo $authorname; ?></h4>
            <p><?php $excerpt = get_the_excerpt(); echo string_limit_words($excerpt,50); ?></p>
         </div>
       <?php endforeach; ?>
       </div>
-				
+
 				  <div class="home-widget grid_7 alpha">
 				  <ul><?php dynamic_sidebar( 'homepage-widget-area' ); ?></ul>
 				  </div>
-				  
+
 				  <div class="grid_8 push_1 alpha">
-<?php 
+<?php
 $args = array( 'taxonomy' => 'series', 'exclude' => '34' );
 //$terms = get_terms('series', $args);
 $terms = apply_filters( 'taxonomy-images-get-terms', null, $args);
@@ -133,29 +133,29 @@ $count = count($terms); $i=0;
 if ($count > 0) {
     foreach ($terms as $term) {
         $i++;
-        $image_url = wp_get_attachment_image( $term->image_id, 'series-list' ); 
+        $image_url = wp_get_attachment_image( $term->image_id, 'series-list' );
     	$term_list .= '<div class="series alignleft"><a href="/series/' . $term->slug . '" title="' . sprintf(__('%s', 'my_localization_domain'), $term->name) . '">'. $image_url.'</a><p><a href="/series/' . $term->slug . '" title="' . sprintf(__('Books in the %s series', 'my_localization_domain'), $term->name) . '">' . $term->name . '</a></p></div>';
     }
     echo $term_list;
 }
-?>		  
+?>
     </div>
-   
-   
- 
-   
-   
-   
-   
-   
-   
-				<?php } else { ?>	
+
+
+
+
+
+
+
+
+
+				<?php } else { ?>
 				<div class="grid_6 push_1 clearfix">
 				  <?php comments_template( '', true ); ?>
 				  </div>
 				<?php } ?>
-				
-				
+
+
 <?php endwhile; ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
